8c7bf853cff9efef0b5d8e35f3c6b8cc50099952
Anwar Hossain <mahedianwar@gmail.com>
2022-08-04 17:43:15 +0600

    design SIdebar

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Body from './Body';
+import Right from './Right';
+import Sidebar from './Sidebar';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main className='flex min-h-screen min-w-max bg-black lg:pb-24'>
+      <Sidebar />
+      <Sidebar />
+      <Body />
+      <Right />
+
+      {/*{showPlayer && (
+        <div className='fixed bottom-0 left-0 right-0 z-50'>
+          <Player accessToken={accessToken} trackUri={playingTrack.uri} />
+        </div>
+      )} */}
+    </main>
+  );
 };
 
 export default Dashboard;
