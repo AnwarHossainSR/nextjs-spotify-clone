fcb16dafbe777b80d597f99247d9a1acf48a0ed2
Anwar Hossain <mahedianwar@gmail.com>
2022-08-04 17:51:44 +0600

    Implement search component

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Search from './Search';
 
 const Body = () => {
   return (
-    <div>Body</div>
-  )
-}
+    <section className='bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5'>
+      <Search />
+    </section>
+  );
+};
 
-export default Body
+export default Body;
