f46b925c7edee262b05f74cc59ac752b469e1555
Anwar Hossain <mahedianwar@gmail.com>
2022-08-13 22:07:36 +0600

    redirected to sign in page if unauthenticated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { useSession } from 'next-auth/react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Dashboard from '../components/Dashboard';
+import Loader from '../components/Loader';
 
 export default function Home() {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/auth/signin');
+    },
+  });
+
+  // Loading animation...
+  if (status === 'loading') {
+    return <Loader />;
+  }
+
   return (
     <div className=''>
       <Head>
